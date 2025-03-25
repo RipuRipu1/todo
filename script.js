63ceb76a91382ca8302db81ac7651296e8fccc79
ripuripu1 <theworrkkers@gmail.com>
2025-03-25 22:59:32 +0700

    trying to fix error missing element

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const deleteButton = document.querySelectorAll('.delete');
-const editButton = document.querySelectorAll('.edit');
 
 deleteButton.forEach(button => {
     button.addEventListener('click', function() {
@@ -8,31 +7,39 @@ deleteButton.forEach(button => {
     });
 });
 
+document.addEventListener('DOMContentLoaded', () => {
+    const editButton = document.querySelectorAll('.edit');
+    editButton.forEach(button => {
+        button.addEventListener('click', function(){
+            const item = this.parentElement;
+            const list_item = item.querySelector('label');
+            
+            if (list_item) {
+                const currentList = list_item.textContent;
+                console.log(currentList);
 
-editButton.forEach(button => {
-    button.addEventListener('click', function(){
-        const item = this.parentElement;
-        const list_item = item.querySelector('label');
-        const currentList = list_item.textContent;
-
-        // input field
-        const input_item = document.createElement('input');
-        input_item.type = 'text';
-        input_item.value = currentList;
-
-        // replace item
-        item.replaceChild(input_item, list_item);
-
-        // add save button
-        this.textContent = 'Save';
-
-        // saving changes
-        this.addEventListener('click', function () {
-            const newList = input_item.value;
-            const newListItem = document.createElement('label');
-            newListItem.textContent = newList;
-            item.replaceChild(newListItem, input_item);
-            this.textContent = 'Edit';
-        }, {once: true});
+                // input field
+                const input_item = document.createElement('input');
+                input_item.type = 'text';
+                input_item.value = currentList;
+        
+                // replace item
+                item.replaceChild(input_item, list_item);
+        
+                // add save button
+                this.textContent = 'Save';
+        
+                // saving changes
+                this.addEventListener('click', function () {
+                    const newList = input_item.value;
+                    const newListItem = document.createElement('label');
+                    newListItem.textContent = newList;
+                    item.replaceChild(newListItem, input_item);
+                    this.textContent = 'Edit';
+                }, {once: true});
+            } else {
+                console.error('Element label not found!');
+            }
+        });
     });
-});
+})
