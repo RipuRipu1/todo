7156e47ffae132df99f3d007785fab1ce1bd4870
ripuripu1 <theworrkkers@gmail.com>
2025-03-25 22:26:36 +0700

    added edit function

--- script.js
+++ script.js
@@ -7,3 +7,32 @@ deleteButton.forEach(button => {
         items.remove();
     });
 });
+
+
+editButton.forEach(button => {
+    button.addEventListener('click', function(){
+        const item = this.parentElement;
+        const list_item = item.querySelector('label');
+        const currentList = list_item.textContent;
+
+        // input field
+        const input_item = document.createElement('input');
+        input_item.type = 'text';
+        input_item.value = currentList;
+
+        // replace item
+        item.replaceChild(input_item, list_item);
+
+        // add save button
+        this.textContent = 'Save';
+
+        // saving changes
+        this.addEventListener('click', function () {
+            const newList = input_item.value;
+            const newListItem = document.createElement('label');
+            newListItem.textContent = newList;
+            item.replaceChild(newListItem, input_item);
+            this.textContent = 'Edit';
+        }, {once: true});
+    });
+});
